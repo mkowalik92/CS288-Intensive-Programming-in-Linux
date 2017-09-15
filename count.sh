e41b4ba12f458bf39a33cafb32fce8f286d08e47
mkowalik92 <mkowalik92@gmail.com>
2017-09-14 22:26:10 -0400

    Update count.sh

--- count.sh
+++ count.sh
@@ -2,21 +2,21 @@
 
 declare -A binCountArray
 
-for y in /bin/[A-Za-z]*;
+for y in /bin/[A-Za-z]*
 do
     comNoBin=${y:5:1}
     #comNoBin=`echo ${y:5:1} | tr ':upper:' 'lower'`
-    if ! [[ "$comNoBin" =~ [^A-Z] ]]; then
-	#
+    if ! [[ "$comNoBin" =~ [^A-Z] ]]
+    then
 	#echo ":Uppercase `echo $comNoBin | tr '[:upper:]' '[:lower:]'` detected:"
-	comNoBinLower=`echo $comNoBin | tr '[:upper:]' '[:lower:]'`
-	let binCountArray[$comNoBinLower]+=1
+	comNoBinLower=$(echo "$comNoBin" | tr '[:upper:]' '[:lower:]')
+	(( binCountArray["$comNoBinLower"]+=1 ))
     else
-	let binCountArray[$comNoBin]+=1
+	(( binCountArray["$comNoBin"]+=1 ))
     fi
 done
 
-for letter in {a..z};
+for letter in {a..z}
 do
-    echo $letter " " ${binCountArray[$letter]}
+    echo $letter "${binCountArray[$letter]}"
 done
